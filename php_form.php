1c89296271a5a2947a679daaee0823d3744e9972
innocent <okamainnocent2020@gmail.com>
2022-05-27 17:59:28 +0100

    modified php_form

--- php_form.php
+++ php_form.php
@@ -23,9 +23,8 @@
 </label><br>
 <label for="gender">
 Gender:
- <input type="radio" name="gender" value="female">Female
-  <input type="radio" name="gender" value="male">Male
-  <input type="radio" name="gender" value="other">Other
+ <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="female">Female
 </label><br>
 <label for="Country">
     Country: <input type="text" name="Country">
